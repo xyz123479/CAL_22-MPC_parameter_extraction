ec21e95dd883f0b1b70261c1714c2ef99ebb865d
Hoyong Jin <jin8495@gmail.com>
2021-12-21 21:37:02 +0900

    zero order path. gpu accelerated. not yet debugged

--- src/residue.py
+++ src/residue.py
@@ -1,4 +1,4 @@
-import numpy as np
+import torch
 
 from tqdm.auto import tqdm
 
@@ -17,7 +17,7 @@ def get_predict_matrix(root_idx, base_idx_table, weight_table, device="cpu"):
 
     return predict_mat
 
-def batched_compute_residue(data, compression_table, batch_size=65536, device="cpu"):
+def compute_residue(data, compression_table, batch_size=65536, device="cpu"):
     root_idx = compression_table['root_idx']
     base_idx_table = compression_table['base_idx_table']
     weight_table = compression_table['weight_table']
@@ -47,54 +47,5 @@ def batched_compute_residue(data, compression_table, batch_size=65536, device="c
 
     residues = torch.concat(residues, dim=0)
     return residues
-
-
-
-
-
-
-
-
-
-
-def predict(line, base_idx_table, weight_table, root_idx):
-    pred_line = []
-    
-    root = line[root_idx]
-    pred_line.append(root)
-    for target_idx in range(len(line)):
-        if target_idx == root_idx:
-            continue
-            
-        weight = weight_table[target_idx]
-        base_idx = base_idx_table[target_idx]
-        base = line[base_idx]
-        pred = (weight * base).astype(line.dtype)
-        
-        pred_line.append(pred)
-    return np.array(pred_line)
-
-def compute_residue(data, compression_table, sel_cluster):
-    root_idx = compression_table['root_idx']
-    base_idx_table = compression_table['base_idx_table']
-    weight_table = compression_table['weight_table']
-
-    residue_list = []
-
-    description = 'Computing residue-%2d/%2d' %(sel_cluster, NUM_CLUSTERS)
-    p_bar = tqdm(total=len(data), desc=description, ncols=150)
-    for line in data:
-        line = line.numpy()
-        pred = predict(line, base_idx_table, weight_table, root_idx)
-        
-        line_without_root = np.concatenate((line[:root_idx], line[root_idx+1:]))
-        residue = np.concatenate((np.expand_dims(line[root_idx], 0),
-                                  line_without_root - pred[1:]))
-        residue_list.append(residue)
-        p_bar.update(1)
-    p_bar.close()
-
-    residue_list = np.array(residue_list)
-    return residue_list
 
 
--- src/scan.py
+++ src/scan.py
@@ -1,27 +1,27 @@
 import numpy as np
+import torch
 
 from tqdm.auto import tqdm
 from src.const import *
-
-SCANNED_SYMBOL_SIZE = 16
+from src.utils import *
 
 def BPX(data, consecutive_xor=True):
     '''
     prediction root = bit_plane[:, :, 0]
     xor base = bit_plane[:, 0, :] / consecutive xor base = bit_plane[:, :-1, :]
     '''
-    block_size = data.shape[1]
-    symbol_size = data.dtype.itemsize
+    if isinstance(data, torch.Tensor):
+        data = data.cpu().numpy()
     
-    num_blocks = len(data)
-    
-    binary_data = np.unpackbits(data, axis=1).reshape(num_blocks, block_size, symbol_size * 8)
+    binary_data = np.unpackbits(data, axis=1).reshape(len(data), LINESIZE, DTYPE_SIZE)
     bit_plane = np.moveaxis(binary_data, 1, -1)
     
     if consecutive_xor:
         bit_plane[:, 1:, 1:] = bit_plane[:, 1:, 1:] ^ bit_plane[:, :-1, 1:] # consecutive xor
     else:
         bit_plane[:, 1:, 1:] = bit_plane[:, 1:, 1:] ^ np.expand_dims(bit_plane[:, 0, 1:], 1) # base xor
+
+    bit_plane = torch.from_numpy(bit_plane)
     return bit_plane
 
 def phi_scan(data, sel_cluster):
@@ -68,3 +68,58 @@ def phi_scan(data, sel_cluster):
     p_bar.close()
     return (scanned_rows, scanned_cols)
     
+def search_idx(data, batch_size, device):
+    num_lines = len(data)
+    rows = data.shape[1]
+    cols = data.shape[2]
+
+    total_one_count_table = torch.zeros(size=(rows, cols), dtype=int, device=device)
+    for minibatch in iter_batch(data, batch_size):
+        minibatch = minibatch.to(device)
+        one_count_table = torch.count_nonzero(minibatch, dim=0)
+        total_one_count_table = total_one_count_table + one_count_table
+    total_zero_count_table = num_lines - total_one_count_table
+    total_zero_count_table = total_zero_count_table.cpu().numpy()
+
+    sorted_index = np.unravel_index(
+            np.argsort(total_zero_count_table, axis=None)[::-1], total_zero_count_table.shape)
+    return sorted_index
+
+def batched_phi_scan(data, batch_size=65536, device="cpu"):
+    num_lines = len(data)
+    rows = data.shape[1]
+    cols = data.shape[2]
+
+    index_checklist = np.zeros(shape=(rows, cols), dtype=int)
+    scanned_rows = []
+    scanned_cols = []
+
+    p_bar = tqdm(total=rows*cols, desc="Searching High Zero Prob Order", ncols=150)
+
+    # start idx
+    sorted_index = search_idx(data, batch_size, device)
+    start_index = (sorted_index[0][0], sorted_index[1][0])
+
+    # start idx check
+    index_checklist[start_index] = 1
+    scanned_rows.append(start_index[0])  # scanned row
+    scanned_cols.append(start_index[1])  # scanned col
+    p_bar.update(1)
+
+    # high zero prob route search
+    prev_index = start_index
+    for _ in range(rows * cols - 1):
+        data_subset = data[data[:, prev_index[0], prev_index[1]] == 0]
+        sorted_next_index_candidates = search_idx(data_subset, batch_size, device) 
+        for i in range(len(sorted_next_index_candidates[0])):
+            index = (sorted_next_index_candidates[0][i], sorted_next_index_candidates[1][i])
+            if index_checklist[index] == 0:
+                index_checklist[index] = 1
+                scanned_rows.append(index[0])  # scanned row
+                scanned_cols.append(index[1])  # scanned col
+                prev_index = index
+                break
+        p_bar.update(1)
+    p_bar.close()
+    return scanned_rows, scanned_cols
+
